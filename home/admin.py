cdce28567a739c09845d3c820bd1fc1c9c5aa872
crowdbotics-dev <engineering@crowdbotics.com>
2023-04-02 21:05:20 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Newmodel
+admin.site.register(Newmodel)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Newmodel(models.Model):
+    'Generated Model'
+    value = models.TextField()
